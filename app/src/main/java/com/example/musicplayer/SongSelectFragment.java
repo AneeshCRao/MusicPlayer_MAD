1ce88e2fd202aa6668f8787b0cd934b492896800
Aneesh <acr1997@gmail.com>
2019-04-17 20:47:02 +0530

    Initial commit

---
+++ app/src/main/java/com/example/musicplayer/SongSelectFragment.java
@@ -0,0 +1,20 @@
+package com.example.musicplayer;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SongSelectFragment extends Fragment {
+    private static final String TAG = "SongSelectFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.song_select, container, false);
+        return view;
+    }
+}

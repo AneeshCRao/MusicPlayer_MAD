3f2cc434ea1df6dc1fb95bb0e867b9bd47c69f27
shobhit-coder <shobhit1998@yahoo.co.in>
2019-04-17 22:47:58 +0530

    added guess basic

---
+++ app/src/main/java/com/example/musicplayer/GuessSongClass.java
@@ -0,0 +1,70 @@
+package com.example.musicplayer;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class GuessSongClass {
+    public void getDetails(Context context, final TextView tv,String lyric,String artist){
+
+// ...
+
+// Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(context);
+        String url ="http://api.musixmatch.com/ws/1.1/track.search?apikey=91d9d078ea2fcb46055848af9cf2b40f&q_lyrics="+lyric+"&q_artist="+artist+"&page_size=3&page=1&s_track_rating=desc";
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+//                        tv.setText(response);
+                        String ans="";
+                        try{
+                            JSONObject jo = new JSONObject(response);
+                            JSONArray songs =(JSONArray) ((JSONObject)((JSONObject)jo.get("message")).get("body")).get("track_list");
+
+                            for(int i=0;i<songs.length();i++){
+                                JSONObject jo1 = (JSONObject) (((JSONObject)(songs.get(i))).get("track"));
+
+                                String tname=(String)jo1.get("track_name");
+                                String alname=(String)jo1.get("album_name");
+                                String arname=(String)jo1.get("artist_name");
+
+                                ans=ans+"Track: "+tname+"\nAlbum: "+alname+"\nArtist: "+arname+"\n\n";
+
+
+                            }
+
+                            tv.setText(ans);
+
+                        }
+                        catch (Exception e){
+                            Log.e("guess_error1",e.toString());
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //textView.setText("That didn't work!");
+
+                tv.setText("error. Check what you entered.");
+                Log.e("guess_error",error.toString());
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+}
--- app/src/main/java/com/example/musicplayer/GuessSongFragment.java
+++ app/src/main/java/com/example/musicplayer/GuessSongFragment.java
@@ -7,14 +7,34 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class GuessSongFragment extends Fragment {
+public class GuessSongFragment extends Fragment implements View.OnClickListener {
     private static final String TAG = "GuessSongFragment";
-
+    TextView tv;
+    EditText ly,ar;
+    Button bt;
+    String lyric="",artist="";
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.guess_song, container, false);
+
+        tv=view.findViewById(R.id.guess_result);
+        ly=view.findViewById(R.id.guess_lyric);
+        ar=view.findViewById(R.id.guess_artist);
+        bt=view.findViewById(R.id.guess_bt);
+        bt.setOnClickListener(this);
         return view;
+    }
+
+
+
+    @Override
+    public void onClick(View view) {
+        GuessSongClass gsc = new GuessSongClass();
+        gsc.getDetails(getContext(),tv,ly.getText().toString(),ar.getText().toString());
     }
 }
